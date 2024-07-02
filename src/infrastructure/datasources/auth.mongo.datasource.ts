3b7409cce98fa0f1cfdd4ae9c8b3f20cce34d5d2
Israel <israel.pe@outlook.es>
2024-07-02 13:54:53 -0500

    refactor:  auth mongo datasource, added hash, compare, mapper, mongo

---
+++ src/infrastructure/datasources/auth.mongo.datasource.ts
@@ -0,0 +1,55 @@
+import { AuthDataSource } from "../../domain/datasources/auth.datasource";
+import { CustomError, RegisterUserDto, UserEntity } from "../../domain";
+import { UserModel } from "../../data/mongodb";
+import { BcryptAdapter } from "../../config";
+import { UsserMapper } from "../mappers/usser.mapper";
+
+type HashFunction = (password: string) => string;
+type CompareFunction = (password: string, hashed: string) => boolean;
+type MapperFunction = (object: { [key: string]: any }) => UserEntity;
+
+export class AuthMongoDatasource implements AuthDataSource {
+
+  constructor(
+      private readonly hashPassword: HashFunction = BcryptAdapter.hash,
+      private readonly comparePassword: CompareFunction = BcryptAdapter.compare,
+      private readonly userMapper: MapperFunction = UsserMapper.userEntityFromObject
+  ) {
+
+  }
+
+  async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
+
+
+    const {name, email, password} = registerUserDto;
+
+    try {
+
+      // 1. verificar si el correo existe
+      const exists = await UserModel.findOne({email: email});
+      if (exists) throw CustomError.badRequest('Verify your credentials');
+
+      const user = await UserModel.create({
+        name,
+        email,
+        // 2. hash de la contrasena
+        password: this.hashPassword(password)
+      })
+
+
+      await user.save();
+      // 3. Mapear la respuesta a la entidad
+      return this.userMapper(user);
+
+    } catch (error) {
+
+      if (error instanceof CustomError) {
+        throw error;
+      }
+
+      throw CustomError.internalServer()
+
+    }
+  }
+
+}
