defc663392efa61f17697eb29bbe362dd3b1ffa8
Israel <israel.pe@outlook.es>
2024-07-02 13:55:28 -0500

    refactor:  change authDataSourceimpl to authMongoSourceimpl

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
-import { AuthDatasourceImpl, AuthRepositoryImpl } from "../../infrastructure";
+import { AuthMongoDatasource, AuthRepositoryImpl } from "../../infrastructure";
 
 export class AuthRoutes {
   static get routes(): Router {
 
     const router = Router();
 
-    const database = new AuthDatasourceImpl();
+    const database = new AuthMongoDatasource();
     const authRepository = new AuthRepositoryImpl(database);
     const controller = new AuthController(authRepository);
 
