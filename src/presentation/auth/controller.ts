a3840935149045918afb62d4e73b4ef93e607655
Israel <israel.pe@outlook.es>
2024-07-02 13:54:02 -0500

    feat: handleErrors

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { AuthRepository, RegisterUserDto } from "../../domain";
+import { AuthRepository, CustomError, RegisterUserDto } from "../../domain";
 
 export class AuthController {
 
@@ -8,13 +8,19 @@ export class AuthController {
   ) {
   }
 
+  private handleError = (error: unknown, res: Response) => {
+    if (error instanceof CustomError) return res.status(error.statusCode).json({error: error.message});
+    console.log(error);//Wiston
+    return res.status(500).json({error});
+  }
+
   registerUser = (req: Request, res: Response) => {
     const [error, registerUserDto] = RegisterUserDto.create(req.body);
     if (error) return res.status(400).json({error});
     this.authRepository.register(registerUserDto!)
         .then(user => res.json(user))
         .catch(error => {
-          res.status(500).json(error)
+          this.handleError(error, res)
         })
 
   }
