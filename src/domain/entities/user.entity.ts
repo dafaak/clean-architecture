7553874c7cb826f147fe8a4bac597b34ee11119a
Israel <israel.pe@outlook.es>
2024-07-02 13:54:11 -0500

    refactor: user entity

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -1,12 +1,26 @@
 export class UserEntity {
-  constructor(
-      public id: string,
-      public name: string,
-      public email: string,
-      public password: string,
-      public role: string[],
-      public img?: string,
-  ) {
+  public id: string;
+  public name: string;
+  public email: string;
+  public password: string;
+  public roles: string[];
+  public img?: string;
 
+  constructor(
+      params: {
+        id: string,
+        name: string,
+        email: string,
+        password: string,
+        roles: string[],
+        img?: string,
+      }
+  ) {
+    this.id = params.id;
+    this.name = params.name
+    this.email = params.email
+    this.password = params.password
+    this.roles = params.roles
+    this.img = params.img
   }
 }
